1935fdb0796fb7564b0e8c8662fce2c01bcbb91f
Lin <1014809521@qq.com>
2021-03-29 21:32:14 +0800

    329

--- src/main/java/com/gpj/controller/doctor/DoctorUserController.java
+++ src/main/java/com/gpj/controller/doctor/DoctorUserController.java
@@ -1,9 +1,11 @@
 package com.gpj.controller.doctor;
 
 import com.gpj.dao.DoctorDao;
+import com.gpj.dao.PatientDao;
 import com.gpj.entity.Authority;
 import com.gpj.entity.Doctor;
 import com.gpj.entity.Patient;
+import com.gpj.entity.Registration;
 import com.gpj.result.PatientQueryResult;
 import com.gpj.result.ResponseResult;
 import com.gpj.service.DoctorService;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.jar.JarOutputStream;
 
 @Controller
 @RequestMapping("/doctor")
@@ -34,6 +37,8 @@ public class DoctorUserController {
     @Autowired
     private RegistrationService registrationService;
     @Autowired
+    private PatientDao patientDao;
+    @Autowired
     private DoctorDao doctorDao;
     @Autowired
     private SQLManager sqlManager;
@@ -41,10 +46,18 @@ public class DoctorUserController {
     @RequestMapping("/index")
     public String index(PatientQueryResult patientQueryResult, HttpSession session,Model model){
         Authority authority = (Authority)session.getAttribute("authority");
-        PageQuery <Patient> page = patientService.findPatientList(patientQueryResult);
+        PageQuery <Registration> page = registrationService.findList(patientQueryResult);
         model.addAttribute("page",page);
+        model.addAttribute("pageNum",patientQueryResult.getPageNum());
         return "doctorIndex";
     }
+    //就医页面
+    @RequestMapping("/seek")
+    public String seek(@RequestParam("id")Integer id,Model model){
+        model.addAttribute("patient",patientDao.single(id));
+        return "seek";
+    }
+
 
     //根据部门查询医生
 @ResponseBody
